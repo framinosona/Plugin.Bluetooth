cc9f06730a2eb6540e38cccf3b103eae4f93c41c
Francois Raminosona <francois.raminosona@laerdal.com>
2025-10-19 22:01:30 +0300

    feat(helpers): add assembly resolution utility
    Introduces a helper for retrieving loaded assemblies by name.
    Facilitates dynamic assembly inspection and management in the plugin.

---
+++ Plugin.Bluetooth/Helpers/AssemblyHelpers.cs
@@ -0,0 +1,25 @@
+
+using System.Reflection;
+
+namespace Plugin.Bluetooth.Helpers;
+
+/// <summary>
+///   Helper methods for working with assemblies.
+/// </summary>
+public static class AssemblyHelpers
+{
+    /// <summary>
+    ///   Gets an assembly from its name.
+    /// </summary>
+    /// <param name="assemblyName"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static Assembly GetAssemblyFromName(string assemblyName)
+    {
+        ArgumentNullException.ThrowIfNull(assemblyName, nameof(assemblyName));
+
+        return AppDomain.CurrentDomain.GetAssemblies()
+            .FirstOrDefault(a => a.GetName().Name == assemblyName)
+               ?? throw new InvalidOperationException($"Assembly with name '{assemblyName}' not found in current AppDomain.");
+    }
+}
