d8eccfed95d8e820f6fccfed53d7501746aadd18
Francois Raminosona <francois.raminosona@laerdal.com>
2025-11-10 13:50:29 +0100

    refa (plugin.bluetooth): remove legacy bluetooth manager and exceptions
    Removes obsolete Bluetooth manager interfaces, base classes, constants, exceptions, and helper methods to simplify codebase and reduce maintenance overhead.

--- Plugin.Bluetooth/Constants/BluetoothSigConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Plugin.Bluetooth.BluetoothSigSpecific;
-
-public static class BluetoothSigConstants
-{
-    /// <summary>Bluetooth Gatt Standard Guid Extension</summary>
-    public readonly static string StandardGuidExtension = "-0000-1000-8000-00805f9b34fb".ToUpperInvariant();
-}
