cb0bcb46122392adc4a9eb7f2ba2983460d356b3
Francois Raminosona <francois.raminosona@laerdal.com>
2025-11-10 13:52:12 +0100

    feat (baseclasses): add async property change wait helpers
    Adds methods for asynchronously waiting on property value changes or conditions with optional timeouts and cancellation support. Improves bindable object usability by enabling awaitable property monitoring.

--- Plugin.Bluetooth/BaseClasses/BaseBindableObject.cs
+++ Plugin.Bluetooth/BaseClasses/BaseBindableObject.cs
@@ -1,10 +1,4 @@
-using System.Collections.Concurrent;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-using Microsoft.Extensions.Logging;
-
-namespace Plugin.Bluetooth;
+namespace Plugin.Bluetooth.BaseClasses;
 
 /// <summary>
 ///     Provides a base class for bindable objects with property change notification, value storage.
@@ -167,4 +161,236 @@ public abstract class BaseBindableObject : INotifyPropertyChanged
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+
+    /// <summary>
+    ///     Waits asynchronously until the specified property equals the expected value, or until the timeout expires.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
+    /// <param name="propertyName">The name of the property to monitor.</param>
+    /// <param name="expectedValue">The value to wait for.</param>
+    /// <param name="timeout">The timeout for this operation</param>
+    /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+    /// <returns>A task that completes when the property equals the expected value or the timeout expires.</returns>
+    protected ValueTask WaitForPropertyToBeOfValue<T>(string propertyName, T expectedValue, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+    {
+        return WaitForPropertyToBe<T>(propertyName, arg => Equals(arg, expectedValue), timeout, cancellationToken);
+    }
+
+
+    /// <summary>
+    ///     Waits asynchronously until the specified property does not equal the unwanted value, or until the timeout expires.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
+    /// <param name="propertyName">The name of the property to monitor.</param>
+    /// <param name="unwantedValue">The value to avoid.</param>
+    /// <param name="timeout">The timeout for this operation</param>
+    /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+    /// <returns>A task that completes when the property does not equal the unwanted value or the timeout expires.</returns>
+    protected ValueTask WaitForPropertyToBeDifferentThanValue<T>(string propertyName, T unwantedValue, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+    {
+        return WaitForPropertyToBe<T>(propertyName, arg => !Equals(arg, unwantedValue), timeout, cancellationToken);
+    }
+
+
+    /// <summary>
+    ///     Waits asynchronously until the specified property satisfies the given condition, or until the timeout expires.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
+    /// <param name="propertyName">The name of the property to monitor.</param>
+    /// <param name="condition">A function that determines whether the property value satisfies the condition.</param>
+    /// <param name="timeout">The timeout for this operation</param>
+    /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+    /// <returns>A task that completes when the property satisfies the condition or the timeout expires.</returns>
+    /// <exception cref="ArgumentException">If the property name is null or whitespace.</exception>
+    protected async ValueTask WaitForPropertyToBe<T>(string propertyName, Func<T?, bool> condition, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(condition);
+        var propertyInfo = GetPropertyInfo(this, propertyName);
+        var propertyValue = GetPropertyValue<T>(this, propertyInfo);
+
+        if (condition.Invoke(propertyValue))
+        {
+            return;
+        }
+
+        var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        try
+        {
+            PropertyChanged += BindableObjectPropertyChanged;
+            await tcs.Task.WaitBetterAsync(timeout, cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            PropertyChanged -= BindableObjectPropertyChanged; //this is needed in the case of a timeout
+        }
+
+        return;
+
+        void BindableObjectPropertyChanged(object? obj, PropertyChangedEventArgs ea)
+        {
+            try
+            {
+                if (ea.PropertyName != propertyName)
+                {
+                    return;
+                }
+
+                var value = GetPropertyValue<T>(obj!, propertyInfo);
+                if (!condition.Invoke(value))
+                {
+                    return;
+                }
+
+                PropertyChanged -= BindableObjectPropertyChanged;
+                tcs.TrySetResult();
+            }
+            catch (Exception ex)
+            {
+                PropertyChanged -= BindableObjectPropertyChanged;
+                tcs.TrySetException(ex);
+            }
+        }
+
+        static PropertyInfo GetPropertyInfo(object obj, string propertyName)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName, nameof(propertyName));
+            ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+
+            var propertyInfo = obj.GetType().GetProperty(propertyName);
+            return propertyInfo == null ? throw new ArgumentException($"Property '{propertyName}' not found on type '{obj.GetType().FullName}'") : propertyInfo;
+        }
+
+        static TP? GetPropertyValue<TP>(object obj, PropertyInfo propertyInfo)
+        {
+            return (TP?)propertyInfo.GetValue(obj, null);
+        }
+    }
+
+
+    /// <summary>
+    ///     Waits asynchronously until the property value changes, or until the timeout expires.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
+    /// <param name="propertyName">The name of the property to monitor.</param>
+    /// <param name="timeout">The timeout for this operation</param>
+    /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+    /// <returns>A task that completes when the property satisfies the condition or the timeout expires.</returns>
+    /// <exception cref="ArgumentException">If the property name is null or whitespace.</exception>
+    protected async ValueTask<T?> WaitForPropertyToChange<T>(string propertyName, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+    {
+        var propertyInfo = GetPropertyInfo(this, propertyName);
+        var tcs = new TaskCompletionSource<T?>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        try
+        {
+            PropertyChanged += BindableObjectPropertyChanged;
+            return await tcs.Task.WaitBetterAsync(timeout, cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            PropertyChanged -= BindableObjectPropertyChanged; //this is needed in the case of a timeout
+        }
+
+        void BindableObjectPropertyChanged(object? obj, PropertyChangedEventArgs ea)
+        {
+            try
+            {
+                if (ea.PropertyName != propertyName)
+                {
+                    return;
+                }
+
+                var value = GetPropertyValue<T>(obj!, propertyInfo);
+
+                PropertyChanged -= BindableObjectPropertyChanged;
+                tcs.TrySetResult(value);
+            }
+            catch (Exception ex)
+            {
+                PropertyChanged -= BindableObjectPropertyChanged;
+                tcs.TrySetException(ex);
+            }
+        }
+
+        static PropertyInfo GetPropertyInfo(object obj, string propertyName)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName, nameof(propertyName));
+            ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+
+            var propertyInfo = obj.GetType().GetProperty(propertyName);
+            return propertyInfo == null ? throw new ArgumentException($"Property '{propertyName}' not found on type '{obj.GetType().FullName}'") : propertyInfo;
+        }
+
+        static TP? GetPropertyValue<TP>(object obj, PropertyInfo propertyInfo)
+        {
+            return (TP?)propertyInfo.GetValue(obj, null);
+        }
+    }
+
+    /// <summary>
+    ///     Waits asynchronously until the property value changes to a new value, or until the timeout expires.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
+    /// <param name="propertyName">The name of the property to monitor.</param>
+    /// <param name="timeout">The timeout for this operation</param>
+    /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+    /// <returns>A task that completes when the property satisfies the condition or the timeout expires.</returns>
+    /// <exception cref="ArgumentException">If the property name is null or whitespace.</exception>
+    protected async ValueTask<T> WaitForPropertyToChangeNotNull<T>(string propertyName, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+    {
+        var propertyInfo = GetPropertyInfo(this, propertyName);
+        var tcs = new TaskCompletionSource<T>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        try
+        {
+            PropertyChanged += BindableObjectPropertyChanged;
+            return await tcs.Task.WaitBetterAsync(timeout, cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            PropertyChanged -= BindableObjectPropertyChanged; //this is needed in the case of a timeout
+        }
+
+        void BindableObjectPropertyChanged(object? obj, PropertyChangedEventArgs ea)
+        {
+            try
+            {
+                if (ea.PropertyName != propertyName)
+                {
+                    return;
+                }
+
+                var value = GetPropertyValue<T>(obj!, propertyInfo);
+                if (value == null)
+                {
+                    return;
+                }
+
+                PropertyChanged -= BindableObjectPropertyChanged;
+                tcs.TrySetResult(value);
+            }
+            catch (Exception ex)
+            {
+                PropertyChanged -= BindableObjectPropertyChanged;
+                tcs.TrySetException(ex);
+            }
+        }
+
+        static PropertyInfo GetPropertyInfo(object obj, string propertyName)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName, nameof(propertyName));
+            ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+
+            var propertyInfo = obj.GetType().GetProperty(propertyName);
+            return propertyInfo == null ? throw new ArgumentException($"Property '{propertyName}' not found on type '{obj.GetType().FullName}'") : propertyInfo;
+        }
+
+        static TP? GetPropertyValue<TP>(object obj, PropertyInfo propertyInfo)
+        {
+            return (TP?)propertyInfo.GetValue(obj, null);
+        }
+    }
+
 }
