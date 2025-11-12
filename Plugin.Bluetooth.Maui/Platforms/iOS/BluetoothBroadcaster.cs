b40db5d95a31f0cafbafcf6a3b864c0c3ab86fbf
Francois Raminosona <francois.raminosona@laerdal.com>
2025-11-12 02:20:01 +0100

    feat (platforms/ios): add initial iOS Bluetooth broadcaster stub
    Introduces a new iOS Bluetooth broadcaster implementation with unimplemented method stubs for native functionality and delegate callbacks.
    Prepares the codebase for future platform-specific development of Bluetooth broadcasting features on iOS.

--- Plugin.Bluetooth.Maui/Platforms/iOS/BluetoothBroadcaster.cs
+++ Plugin.Bluetooth.Maui/Platforms/iOS/BluetoothBroadcaster.cs
@@ -1,7 +1,11 @@
+using Plugin.Bluetooth.Maui.PlatformSpecific;
+
 namespace Plugin.Bluetooth.Maui;
 
-public partial class BluetoothBroadcaster : BaseBluetoothBroadcaster
+public partial class BluetoothBroadcaster : BaseBluetoothBroadcaster, CbPeripheralManagerProxy.ICbPeripheralManagerProxyDelegate
 {
+
+    public CbPeripheralManagerProxy? CbPeripheralManagerProxy { get; protected set; }
 
     #region BaseBluetoothBroadcaster
 
@@ -25,21 +29,75 @@ public partial class BluetoothBroadcaster : BaseBluetoothBroadcaster
         throw new NotImplementedException();
     }
 
-    protected override void NativeRefreshIsBroadcasting()
+    protected async override ValueTask NativeInitializeAsync(Dictionary<string, object>? nativeOptions = null)
     {
         throw new NotImplementedException();
     }
 
-    protected async override ValueTask NativeInitializeAsync()
+    public async override Task NativeSetAdvertisingDataAsync(Dictionary<string, object>? nativeOptions = null)
     {
         throw new NotImplementedException();
     }
-
-    public async override Task NativeSetAdvertisingDataAsync(IEnumerable<Guid> serviceGuids)
-    {
-        throw new NotImplementedException();
-    }
-
 
     #endregion
+
+    #region CbPeripheralManagerProxy.ICbPeripheralManagerProxyDelegate
+
+    public void AdvertisingStarted(NSError? error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CharacteristicSubscribed(CBCentral central, CBCharacteristic characteristic)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CharacteristicUnsubscribed(CBCentral central, CBCharacteristic characteristic)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ServiceAdded(CBService service)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReadRequestReceived(CBATTRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WillRestoreState(NSDictionary dict)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WriteRequestsReceived(CBATTRequest[] requests)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DidOpenL2CapChannel(NSError? error, CBL2CapChannel? channel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DidPublishL2CapChannel(NSError? error, ushort psm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DidUnpublishL2CapChannel(NSError? error, ushort psm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StateUpdated()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
 }
